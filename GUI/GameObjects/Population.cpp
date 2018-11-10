e560323d08ae5b170cf841807a11219a7e9370b4
Steph. A. Carazo <stefany_199829@hotmail.com>
2018-11-10 00:40:33 -0600

    After-merge Fix
    conflictos en CMakeLists y un par de correcciones no contempladas en el merge

--- GUI/GameObjects/Population.cpp
+++ GUI/GameObjects/Population.cpp
@@ -2,20 +2,18 @@
 // Created by jglez2330 on 06/11/18.
 //
 
-#define num_players 1
-
 #include "Population.h"
 #include "../../Algorithms/AStar.h"
 
 Population::Population() {
-    for (int i = 0; i < num_players; i++) {
+    for (int i = 0; i < sizeof(players)/sizeof(*players); i++) {
         this->players[i] = new Player(49 - 3 * i); // Crea 5 jugadores y uno va encima del otro
     }
 
 }
 
 void Population::draw() {
-    for (int i = 0; i < num_players ; i++) {
+    for (int i = 0; i < sizeof(players)/sizeof(*players) ; i++) {
         players[i]->draw(); // Dibuja todos los jugadores
         //players[i]->setPath(nullptr);
     }
@@ -23,7 +21,7 @@ void Population::draw() {
 }
 
 void Population::setPath(Level *gameLevel, int xGraph, int yGraph) {
-    for (int i = 0; i < num_players ; i++) {
+    for (int i = 0; i < sizeof(players)/sizeof(*players) ; i++) {
         players[i]->path = gameLevel->getPath(map, xGraph, yGraph, players[i]->getI(), players[i]->getJ()); // Encuentra un path para un mapa específico para cada uno de los jugadores
         /*
         std::list<Cell<int> *> *path = AStar::findPath(this->map, players[k]->getI(), this->getJ(), i + k, j);
@@ -34,7 +32,7 @@ void Population::setPath(Level *gameLevel, int xGraph, int yGraph) {
 }
 
 void Population::updatePlayers() {
-    for (int i = 0; i < num_players; i++) {
+    for (int i = 0; i < sizeof(players)/sizeof(*players); i++) {
         if(players[i]->path != nullptr && players[i]->path->size() > 0) {
             players[i]->setI(players[i]->path->back()->getXpos()); // La nueva posición en X es la coordenada en X de la próxima casilla a la que se debe dirigir
             players[i]->setJ(players[i]->path->back()->getYpos());// La nueva posición en Y es la coordenada en Y de la próxima casilla a la que se debe dirigir
--- GUI/LevelBuilder.cpp
+++ GUI/LevelBuilder.cpp
@@ -6,14 +6,19 @@
 #include "../Levels/AstarLevel.h"
 #include "../Levels/PrimLevel.h"
 #include "../Levels/KruskalLevel.h"
+#include "../Levels/DijkstraLevel.h"
 
 
 Level* LevelBuilder::getLevel(int levelNumber) {
-    if (levelNumber == 0){
+    if (levelNumber == 0)
         return new AstarLevel();
-    }
+
     else if (levelNumber == 1)
-        return new PrimLevel();
+        return new DijkstraLevel();
+
     else if (levelNumber == 2)
+        return new PrimLevel();
+
+    else if (levelNumber == 3)
         return new KruskalLevel();
 }
